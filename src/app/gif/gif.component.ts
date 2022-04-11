bfc65e2202369d1fd30b59d8ef795bb29f930213
Neha Wagadre <neha.wagadre19@gmail.com>
2022-04-12 00:29:56 +0530

    code cleanup

--- src/app/gif/gif.component.ts
+++ src/app/gif/gif.component.ts
@@ -8,90 +8,95 @@ import { LoadingBarService } from '@ngx-loading-bar/core';
   templateUrl: './gif.component.html',
   styleUrls: ['./gif.component.css']
 })
+
+
 export class GifComponent implements OnInit {
-  searchedText:String=""
-  ApiLoading:boolean=false
-  searchList:any;
-  limit:number=10
-  offset:number=0
+
+
+  searchedText: String = ""
+  ApiLoading: boolean = false
+  searchList: any;
+  limit: number = 10
+  offset: number = 0
+
+
+
   @ViewChild('inputSearch') inputSearch: ElementRef | undefined;
-  constructor(private search: SearchService,private loadingBar: LoadingBarService) {
-    
-   }
+
+  constructor(private search: SearchService, private loadingBar: LoadingBarService) { }
 
   ngOnInit(): void {
     this.searchGifs()
   }
 
+
   @HostListener('window:scroll')
   onScroll() {
-     if(this.ApiLoading){
-        console.log("Api Already loading hence returning back")
-        return
-     }
+    if (this.ApiLoading) {
+      console.log("Api Already loading hence returning back")
+      return
+    }
 
     if ((window.innerHeight + window.scrollY) >= document.body.scrollHeight) {
-      this.offset += 10; 
+      this.offset += 10;
       this.loadingBar.start();
-      if(this.searchedText.length == 0){
+
+      if (this.searchedText.length == 0) {
         this.searchGifs()
       }
-      else{
+      else {
         this.searchQueryWithText(this.searchedText)
       }
     }
   }
 
   searchGifs() {
-
     console.log("search function works");
-    this.ApiLoading=true
-    this.search.searchApi(this.offset,this.limit).subscribe((result: any) => {
+    this.ApiLoading = true
+    this.search.searchApi(this.offset, this.limit).subscribe((result: any) => {
       this.replaceAndAppendArray(result)
     })
-
   }
-  searchQuery(text:any){
+
+
+
+  searchQuery(text: any) {
     this.loadingBar.start();
-    this.searchedText=text
-    this.searchList=[]
-    this.offset=0
+    this.searchedText = text
+    this.searchList = []
+    this.offset = 0
     console.log(this.inputSearch?.nativeElement.value)
-    this.ApiLoading=true
-    this.search.searchApiWithQuery(text,this.offset,this.limit).subscribe((result: any) => {
+    this.ApiLoading = true
+
+    this.search.searchApiWithQuery(text, this.offset, this.limit).subscribe((result: any) => {
       console.log(result);
       this.searchList = result.data;
       console.log("searchList", this.searchList);
       this.loadingBar.stop();
 
     })
-
   }
-  searchQueryWithText(text:any){
+
+
+
+  searchQueryWithText(text: any) {
     console.log(this.inputSearch?.nativeElement.value)
-    this.ApiLoading=true
-    this.search.searchApiWithQuery(text,this.offset,this.limit).subscribe((result: any) => {
+    this.ApiLoading = true
+    this.search.searchApiWithQuery(text, this.offset, this.limit).subscribe((result: any) => {
       this.replaceAndAppendArray(result)
     })
-
   }
 
-  replaceAndAppendArray(result:any){
+
+  replaceAndAppendArray(result: any) {
     console.log(result);
-    var arr = undefined
-    if(this.searchList != undefined){
-      arr = this.searchList as GifyPoJo[]
-    }
-    console.log("-----------",arr)
-    if(arr ==undefined || arr!.length == 0){
-      this.searchList = result.data;
+    if(this.searchList == undefined){
+      this.searchList=result.data;
     }
     else{
-      console.log("other times loading",result.data)
-      this.searchList.push(...result.data)
+      this.searchList.push(...result.data);
     }
-    console.log("searchList", this.searchList);
-    this.ApiLoading=false
+    this.ApiLoading = false
     this.loadingBar.stop();
   }
 
